eb218d15c4eb0e41e158aa42e993360f31803201
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2022-05-06 15:23:38 -0700

    Components added
    Finished mobile images component and adjusted their design for mobile.

--- mmrce/src/components/Containers/Mobile/mobile-image.js
+++ mmrce/src/components/Containers/Mobile/mobile-image.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./mobile-image.css";
 let MobImage = (props) => {
   return (
     <div className="mob-img-container">
-      <img src={props.source} />
+      <img className="mob-img-source" src={props.source} />
     </div>
   );
 };
